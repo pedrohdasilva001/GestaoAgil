72e6c16e64ad2c2eb3a46ce8e062b2f69b884d15
Pedro Henrique <pedrohenrique3060156@gmail.com>
2025-11-08 11:21:20 -0300

    Finalização do Projeto

---
+++ Login.js
@@ -0,0 +1,12 @@
+function criacao() {
+    window.location.href = "signin.html";
+}
+function login() {
+    window.location.href = "index.html";
+}
+function painel() {
+    window.location.href = "painel.html";
+}
+function forget() {
+    window.location.href = "forget.html";
+}
